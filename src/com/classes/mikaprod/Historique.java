32e8233504f0866cdc68865fef0508632225631b
RobinAlex <robin.alexandre49@hotmail.fr>
2013-12-17 12:12:56 +0100

    ajout methode AjoutLigneHistorique

--- src/com/classes/mikaprod/Historique.java
+++ src/com/classes/mikaprod/Historique.java
@@ -1,5 +1,8 @@
 package com.classes.mikaprod;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Historique {
 	
 	private Utilisateur utilisateur;
@@ -64,6 +67,47 @@ public class Historique {
 		this.dateDebut = dateDebut;
 		this.dateFin = dateFin;
 	}
+	
+	//METHODES
+	public Boolean AjouterLigneHistorique(Poste poste, Utilisateur utilisateur, Produit produit, Boolean entree) {
+		
+		Boolean status = false;
+		Date dateActuelle = new Date();
+		SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy kk:mm:ss"); 
+		
+		if (entree) {
+			// Si entree == true -> Ajout d'une ligne d'entree
+			this.dateDebut = formatDate.format(dateActuelle);
+		} else if (!entree) {
+			// Si entree == false -> Ajout d'une ligne de fin
+			this.dateFin = formatDate.format(dateActuelle);
+		} else {
+			// Sinon status de la methode false
+			status = false;
+		}
+		
+		if (poste != null) {
+			this.poste = poste;
+		} else {
+			status = false;
+		}
+		
+		if (utilisateur != null) {
+			this.utilisateur = utilisateur;
+		} else {
+			status = false;
+		}
+		
+		if (produit != null) {
+			this.produit = produit;
+		} else {
+			status = false;
+		}		
+		
+		return status;
+		
+	}
+	
 	
 	
 	
