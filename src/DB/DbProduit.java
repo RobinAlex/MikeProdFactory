669f808efb7e8457a0a0739ea6f13b4374f948ed
Andrei <gpandrei@gmail.com>
2013-12-18 14:08:32 +0100

    Fix de quelques classes DbXxxx

--- src/DB/DbProduit.java
+++ src/DB/DbProduit.java
@@ -2,4 +2,12 @@ package DB;
 
 public class DbProduit {
 
+	public static final String TABLE_NAME = "Produit";
+	public static final String COL_ID = "id_produit";
+	public static final String COL_ID_COMMANDE = "id_commande";
+	public static final String COL_ID_POSTE = "id_poste";
+	public static final String COL_FLAG_ATTENTE = "flag_attente";
+	public static final String COL_FLAG_TERMINE = "flag_termine";
+
+	
 }
