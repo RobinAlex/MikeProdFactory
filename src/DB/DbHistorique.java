e1508c6dfc20d85c5f1cfc52330677b77b90a84a
Andrei <gpandrei@gmail.com>
2013-12-18 12:33:53 +0100

    fix sql

--- src/DB/DbHistorique.java
+++ src/DB/DbHistorique.java
@@ -1,5 +1,11 @@
 package DB;
 
 public class DbHistorique {
-
+	public static final String TABLE_NAME = "Historique";
+	public static final String COL_ID = "id_historique";
+	public static final String COL_ID_POSTE = "id_poste";
+	public static final String COL_ID_UTILISATEUR = "id_utilisateur";
+	public static final String COL_ID_PRODUIT = "id_produit";
+	public static final String COL_DATE_DEBUT = "date_Debut";
+	public static final String COL_DATE_FIN = "date_Fin";
 }
