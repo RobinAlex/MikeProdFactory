f73bffbb316f334f07fd132e7d22463354ea3444
RobinAlex <robin.alexandre49@hotmail.fr>
2013-12-19 14:01:32 +0100

    ajout menu contextuel

--- src/activities/FileAttenteActivity.java
+++ src/activities/FileAttenteActivity.java
@@ -17,7 +17,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
